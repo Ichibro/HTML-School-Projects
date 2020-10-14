ec0e34e76852035d33c9b4cfe59182c8e281c815
Ichibro <72879605+Ichibro@users.noreply.github.com>
2020-10-14 12:17:24 -0400

    Add files via upload

---
+++ Ichiro Pineiro Myspace/Javascripto.js
@@ -0,0 +1,8 @@
+function pop(){
+	var User=document.getElementById("user").value; 
+	var Pass=document.getElementById("pass").value;
+     if (User=="Mega" & Pass=="123") {
+		 location.href="Bootstrapp.html";
+	 }	 	
+}
+
